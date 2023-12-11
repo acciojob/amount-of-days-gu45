94e94618145bb5ce13e698a16deab2a355a675a9
gu45 <86247735+gu45@users.noreply.github.com>
2023-12-11 21:49:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function daysOfAYear(yer){
+    if(yer%4==0){
+        if(yer%100==0){
+            if(yer%400==0){
+                return 366
+            }
+            else{
+                return 365
+            }
+        }
+        return 366
+    }
+    else{
+        return 365
+    }
+}
